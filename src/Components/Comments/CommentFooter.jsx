b59b57870bac6420a7f92728ff5aa7bd2b33750b
CristiYTRomania <163294932+CristiYTRomania@users.noreply.github.com>
2025-05-03 10:36:30 +0300

    Add files via upload

---
+++ src/Components/Comments/CommentFooter.jsx
@@ -0,0 +1,29 @@
+import CommentVotes from "./CommentVotes";
+import CommentBtn from "./CommentBtn";
+
+const CommentFooter = ({
+  vote,
+  setVoted,
+  score,
+  setScore,
+
+  commentData,
+  setReplying,
+  setDeleting,
+  setDeleteModalState,
+  setEditing,
+}) => {
+  return (
+    <div className="comment--footer">
+      <CommentBtn
+        commentData={commentData}
+        setReplying={setReplying}
+        setDeleting={setDeleting}
+        setDeleteModalState={setDeleteModalState}
+        setEditing={setEditing}
+      />
+    </div>
+  );
+};
+
+export default CommentFooter;
